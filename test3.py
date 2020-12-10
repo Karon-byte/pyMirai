ecfb47e759bc7fca3e478173e023cc694d291f54
karon <zeroone182004@gmail.com>
2020-12-11 07:04:09 +0900

    frame remake

--- test3.py
+++ test3.py
@@ -9,6 +9,10 @@ root.title("test")
 btn_t=StringVar()
 btn_shift = False
 subtexts=[]
+
+btn_pad=10
+frame_pad=5
+
 def substext(text):
     if btn_shift:
         btn =btn_t.get() + str(text)
@@ -18,23 +22,24 @@ def substext(text):
     btn_t.set(btn)
 
 def makebtn(test,root,btn_l):
-    btn_s = str(test) + "\nhoge"
+    btn_s = "\n   " + str(test) + "\nhoge"
     btn_l.append(
-        ttk.Button(root,text=btn_s,padding=20,command=lambda:substext(test))
+        ttk.Button(root,text=btn_s,padding=btn_pad,command=lambda:substext(test))
         )
 
 tks=[]
 btns=[]
 opebtns=[]
 
-root_frame=ttk.Frame(root,padding=30)
-frame2=ttk.Frame(root_frame,padding=30)
-frame3=ttk.Frame(root_frame,padding=30)
-frame4=ttk.Frame(root_frame,padding=30)
-frame5=ttk.Frame(root_frame,padding=30)
-opeframe=ttk.Frame(root_frame,padding=30)
-ent1=ttk.Entry(frame2,textvariable=btn_t,width=50)
-
+frame_pad=5
+root_frame=ttk.Frame(root,padding=frame_pad)
+top_frame=ttk.Frame(root_frame,padding=frame_pad)
+frame1=ttk.Frame(root_frame,padding=frame_pad)
+frame2=ttk.Frame(root_frame,padding=frame_pad)
+frame3=ttk.Frame(root_frame,padding=frame_pad)
+frame4=ttk.Frame(root_frame,padding=frame_pad)
+frame5=ttk.Frame(root_frame,padding=frame_pad)
+ent1=ttk.Entry(top_frame,textvariable=btn_t,width=73)
 
 f_frame=frame5
 for x in range(10):
@@ -47,12 +52,12 @@ for x in range(10):
     makebtn(x,f_frame,btns)
     
 
-btns_add=ttk.Button(frame2,text="+",padding=20,command=lambda:substext("+"))
-btns_sub=ttk.Button(frame3,text="-",padding=20,command=lambda:substext("-"))
-btns_multi=ttk.Button(frame4,text="*",padding=20,command=lambda:substext("*"))
-btns_div=ttk.Button(frame5,text="/",padding=20,command=lambda:substext("/"))
-btns_equ=ttk.Button(frame5,text="=",padding=20)
-btns_shift=ttk.Button(frame5,text="SHIFT",padding=20,command=lambda:btn_shift != btn_shift)
+btns_add=ttk.Button(frame2,text="\n+\n",padding=btn_pad,command=lambda:substext("+"))
+btns_sub=ttk.Button(frame3,text="\n-\n",padding=btn_pad,command=lambda:substext("-"))
+btns_multi=ttk.Button(frame4,text="\n*\n",padding=btn_pad,command=lambda:substext("*"))
+btns_div=ttk.Button(frame5,text="\n/\n",padding=btn_pad,command=lambda:substext("/"))
+btns_equ=ttk.Button(frame5,text="\n=\n",padding=btn_pad)
+btns_shift=ttk.Button(frame5,text="\nSHIFT\n",padding=btn_pad,command=lambda:btn_shift != btn_shift)
 
 opebtns.append(btns_add)
 opebtns.append(btns_sub)
@@ -60,22 +65,25 @@ opebtns.append(btns_multi)
 opebtns.append(btns_div)
 
 tks.append(root_frame)
+tks.append(top_frame)
+tks.append(frame1)
 tks.append(frame2)
 tks.append(frame3)
 tks.append(frame4)
 tks.append(frame5)
 
+btn_padx=5
+btn_ipad=5
 for item in tks:
     item.pack()
-
 ent1.pack()
 for x in range(1,10):
-    btns[x].pack(side=LEFT,padx=10)
+    btns[x].pack(side=LEFT,padx=btn_padx,ipadx=btn_ipad)
 for x in opebtns:
-    x.pack(side=RIGHT,padx=10)
-btns_shift.pack(side=LEFT,padx=10)
-btns[0].pack(side=LEFT,padx=10)
-btns_equ.pack(side=LEFT,padx=10)
+    x.pack(side=RIGHT,padx=btn_padx,ipadx=btn_ipad)
+btns_shift.pack(side=LEFT,padx=btn_padx,ipadx=btn_ipad)
+btns[0].pack(side=LEFT,padx=btn_padx,ipadx=btn_ipad)
+btns_equ.pack(side=LEFT,padx=btn_padx,ipadx=btn_ipad)
 
 
 root.mainloop()
---
+++ test5.py
@@ -0,0 +1,48 @@
+﻿from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("test5")
+
+btn_t=StringVar()
+btn_shift = False
+subtexts=[]
+tks=[]
+btns=[]
+opebtns=[]
+fpad=5
+btn_pad=10
+
+def substext(text):
+    if btn_shift:
+        btn =btn_t.get() + str(text)
+        btn_t.set(btn)
+    
+    btn = btn_t.get() + str(text)
+    btn_t.set(btn)
+
+def makebtn(test,root,btn_l):
+    btn_s = "\n   " + str(test) + "\nhoge"
+    btn_l.append(
+        ttk.Button(root,text=btn_s,padding=btn_pad,command=lambda:substext(test))
+        )
+
+root_frame=ttk.Frame(root,padding=fpad)
+f1=ttk.Frame(root_frame,padding=fpad)
+f2=ttk.Frame(root_frame,padding=fpad)
+f2_l=ttk.Frame(f2,padding=fpad)
+f2_r=ttk.Frame(f2,padding=fpad)
+f2_l.grid(row=0,ipadx=150,ipady=200)
+f2_r.grid(row=1,ipadx=100,ipady=200)
+
+for x in range(10):
+    makebtn(x,f2_l,btns)
+
+btns_add=ttk.Button(f2_r,text="\n+\n",padding=btn_pad,command=lambda:substext("+"))
+btns_sub=ttk.Button(f2_r,text="\n-\n",padding=btn_pad,command=lambda:substext("-"))
+btns_multi=ttk.Button(f2_r,text="\n*\n",padding=btn_pad,command=lambda:substext("*"))
+btns_div=ttk.Button(f2_r,text="\n/\n",padding=btn_pad,command=lambda:substext("/"))
+btns_equ=ttk.Button(f2_r,text="\n=\n",padding=btn_pad)
+btns_shift=ttk.Button(f2_r,text="\nSHIFT\n",padding=btn_pad,command=lambda:btn_shift != btn_shift)
+
+tks.append(root_frame)
